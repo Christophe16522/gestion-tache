c08d24161493a45afa660be5a87deae6d3d5dee1
Lai Christophe <lai_christophe@hotmail.com>
2024-12-14 16:18:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState(""); //Etat pour la tache actuelle
+  const [tasks, setTasks] = useState([]); //Liste des taches
+
+  const addTask = () => {
+    if (task.trim() !== "") {
+      setTasks([...tasks, { text: task, completed: false }]);
+      setTask(""); //reinitialiser le champs
+    }
+  };
+
+  const removeTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  const toogleComplete = (index) => {
+    setTasks(
+      tasks.map((t, i) => (i === index ? { ...t, completed: !t.completed } : t))
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Gestionnaire de Tâches</h1>
+      <div>
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Ajouter une tache"
+        />
+        <button onClick={addTask}>Ajouter</button>
+      </div>
+      <ul>
+        {tasks.map((t, index) => (
+          <li
+            key={index}
+            onClick={() => toogleComplete(index)}
+            style={{ cursor: "pointer" }}
+          >
+            <span
+              style={{ textDecoration: t.completed ? "line-through" : "none" }}
+            >
+              {t.text}
+            </span>
+
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                removeTask(index);
+              }}
+            >
+              Supprimer
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
